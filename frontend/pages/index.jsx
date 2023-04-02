9043aa8bbdf80a4c7d743191ff6e1cf877cb0855
raizo07 <agadaraizo@gmail.com>
2023-04-02 22:23:56 +0100

    updates

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,71 +1,27 @@
+import { Contract, providers } from "ethers";
+import { formatEther } from "ethers/lib/utils";
 import Head from "next/head";
-import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import {
+  CRYPTODEVS_DAO_ABI,
+  CRYPTODEVS_DAO_CONTRACT_ADDRESS,
+  CRYPTODEVS_NFT_ABI,
+  CRYPTODEVS_NFT_CONTRACT_ADDRESS,
+} from "../constants";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Your Amazing Project</title>
-        <meta
-          name="description"
-          content="Created by love with Startertemp and LearnWeb3DAO"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  // ETH Balance of the DAO contract 
+  const [treasuryBalance, setTreasuryBalance] = useState("0");
+// Number of proposals created in the DAO 
+const [numProposals setNumProposal] = useState("0");
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="#">My Project</a>
-        </h1>
 
-        <p className={styles.description}>
-          What are you waiting for? Jump and just{" "}
-          <code className={styles.code}>build your code</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a
-            href="https://discord.gg/learnweb3"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>LearnWeb3 Discord &rarr;</h2>
-            <p>Find help from the Community.</p>
-          </a>
 
-          <a
-            href="https://learnweb3.io"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Learn Web3 &rarr;</h2>
-            <p>Learn Web3 Free with LearnWeb3DAO</p>
-          </a>
 
-          <a
-            href="https://github.com/startertemp"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Startertemp &rarr;</h2>
-            <p>Follow the Startertemp project!</p>
-          </a>
 
-          <a
-            href="https://github.com/startertemp/nextjs-hardhat"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Contribute &rarr;</h2>
-            <p>Contribute to the template project.</p>
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+
+
 }
