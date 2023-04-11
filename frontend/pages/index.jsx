cac2f71e3d04e653803dd840910bb8f03cd52bba
raizo07 <agadaraizo@gmail.com>
2023-04-11 10:59:24 +0100

    updated the addresses of both DAO contract and NFT contract along with their ABI's

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -15,7 +15,22 @@ export default function Home() {
   // ETH Balance of the DAO contract 
   const [treasuryBalance, setTreasuryBalance] = useState("0");
 // Number of proposals created in the DAO 
-const [numProposals setNumProposal] = useState("0");
+const [numProposals, setNumProposal] = useState("0");
+// Array of all proposals created in the DAO
+const [proposals, setProposals] = useState([]);
+// User's balance of CryptoDevs NFTs
+const [nftBalance, setNftBalance] = useState("0");
+// Fake NFT Token ID to purchase. Used when creating a proposal.
+const [fakeNftTokenId, setFakeNftTokenId] = useState("");
+// One of "Create Proposal" or "View Proposals"
+const [selectedTab, setSelectedTab] = useState("");
+// True if waiting for a transaction to be mined, false otherwise.
+const [loading, setLoading] = useState(false);
+// True if user has connected their wallet, false otherwise
+const [walletConnected, setWalletConnected] = useState(false);
+// isOwner gets the owner of the contract through the signed address
+const [isOwner, setIsOwner] = useState(false);
+const Web3ModalRef = useRef();
 
 
 
