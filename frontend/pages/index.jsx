05fa0bdaef92a48fb674ec9b3094fc387882ea14
raizo07 <agadaraizo@gmail.com>
2023-04-17 17:28:18 +0100

    updated frontend

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -150,6 +150,48 @@ const fetchProposalById = async (id) => {
   }
 };
 
+// Runs a loop `numProposals` times to fetch all proposals in the DAO
+  // and sets the `proposals` state variable
+
+const fetchAllProposals = async () => {
+  try {
+    const proposals = [];
+    for (let i = 0; i < numProposals; i++) {
+      const proposal = await fetchProposalById(i);
+      proposals.push(proposal);
+    }
+    setProposals(proposals);
+    return proposals;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Calls the `voteOnProposal` function in the contract, using the passed
+  // proposal ID and Vote
+
+const voteOnProposal = async (proposalId, _vote) => {
+  try {
+    const signer = getProviderOrSigner(true);
+    const daoContract = await getDaoContractInstance(signer);
+
+    let vote = _vote === "YAY" ? 0 : 1;
+    const txn = await daoContract.voteOnProposal(proposalId, vote);
+    setLoading(true);
+    await txn.wait();
+    setLoading(false);
+    await fetchAllProposals();
+  } catch (error) {
+    console.error(error);
+    window.alert(error.reason);
+  }
+};
+
+
+
+
+
+
 
 
 
